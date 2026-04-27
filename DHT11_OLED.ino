3bec0f703477917cb2b4fe0bc69cd3559d22e8bc
Julian_One <julian.qehaja@icloud.com>
2026-04-27 22:19:26 +0200

    Update DHT11_OLED.ino
    Changed serial baud rate and updated OLED display scaling and cursor positions. Modified LED control logic for humidity and temperature thresholds.

--- DHT11_OLED.ino
+++ DHT11_OLED.ino
@@ -1,6 +1,11 @@
+//include library packages
+
 #include <GyverOLED.h>
 #include <SimpleDHT.h>
 #include <Wire.h> 
+
+
+//creating variables
 
 int gruen = 16;
 int blau = 23;
@@ -8,13 +13,13 @@ int rot = 15;
 int gruen2 = 18;
 int blau2 = 17;
 int rot2 = 19;
-
 int pinDHT11 = 14;
+
 SimpleDHT11 dht11(pinDHT11);
 GyverOLED<SSH1106_128x64> oled;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   oled.init();
   oled.clear();
 
@@ -31,20 +36,35 @@ void loop() {
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
+
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
     Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
     return;
   }
-  oled.setScale(2);
+
+
+  //OLED display style
+
+  oled.setScale(3);
   oled.setCursor(0,0);
-  oled.print("Humid.:");
+  oled.print("==");
+  oled.setCursor(38,0);
+  //oled.print("Humidity:"); (optional: print Humidity:xx instead of just the number.)
   oled.print(humidity);
-  oled.setCursor(0,2);
-  oled.print("Temp.:");
+  oled.setCursor(75,0);
+  oled.print("%===");
+  oled.setCursor(0,5);
+  oled.print("==");
+  oled.setCursor(38,5);
+  //oled.print("Temperature:"); (optional: print Temperature:xx instead of just the number.)
   oled.print(temperature);
-  oled.setCursor(0,3);
+  oled.setCursor(75,5);
+  oled.print("C===");
   oled.update();
+
+
+  //set all LEDs to LOW
 
 digitalWrite(rot, LOW);
 digitalWrite(blau, LOW);
@@ -53,10 +73,13 @@ digitalWrite(rot2, LOW);
 digitalWrite(blau2, LOW);
 digitalWrite(gruen2, LOW);
 
+
+  //define different conditions for the LEDs
+
   if (humidity < 35){
     digitalWrite(rot, HIGH);
   }
-  else
+  else 
     if (humidity > 70){
       digitalWrite(blau, HIGH);
   }
@@ -67,7 +90,7 @@ digitalWrite(gruen2, LOW);
   if (temperature > 27){
     digitalWrite(rot2, HIGH);
   }
-  else
+  else 
     if (temperature < 17){
       digitalWrite(blau2, HIGH);
   }
